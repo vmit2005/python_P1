5ed49a5083c448cafff9656ca1a31b4eab56abd1
vmit2005 <vmit2005@yandex.ru>
2022-11-20 22:49:18 +0400

    20_11_2022

--- P21DZ.py
+++ P21DZ.py
@@ -1,15 +1,14 @@
 import re
 print('Задание 1')
+
 dat=[]
 while dat==[]:
     d=input("Введите дату в формате dd-mm-yyyy: ")
     #d='32-12-2005'
-    reg=r'^((0\d|1\d|2\d|3[0-1])-(0\d|1[0-2])-(19|20)\d*)$'
-    dat=re.findall(reg, d)
-    #dat1=(re.search(reg, d).group())
-
+    reg= r"(0\d|1\d|2\d|3[0-1])-(0\d|1[0-2])-(19|20|21)\d{2}"
+    dat=re.findall(reg,d)
+    if dat==[]:
+        print ("Неверный формат данных или данные не входят в разрешенный диапазон.")
 
 print('Введенная дата',dat)
-print()
-print()
-print('Задание 2')
+
